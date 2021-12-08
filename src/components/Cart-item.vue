b6958b0400bd13bd2800c3d8ec2df5530c301de1
Dmitriy <severenchuk.dmitriy@point2web.com>
2021-12-08 21:13:02 +0200

    cart-item markup

--- src/components/Cart-item.vue
+++ src/components/Cart-item.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="cart-item">
-    <div class="cart__image__wrapper">
+  <div class="cart-item" @click="showQuantity">
+    <div class="cart-item__image__wrapper">
       <img
         :src="require('@/assets/images/Catalog/' + cartItemData.image)"
         alt="cloth"
-        class="card__content-item"
+        class="cart-item__image"
       />
+    </div>
+    <div class="cart-item__content">
+      <div class="cart-item__content__info">
+        <a href="" class="cart-item__title-link">
+          <h3 class="cart-item__title">{{ cartItemData.title }}</h3>
+          <div class="cart-item__size">(SIZE)</div>
+        </a>
+        <div class="cart-item__price">{{ cartItemData.price }}.00 ₽</div>
+        <div class="cart-item__counter">
+          <div class="cart-item__counter__wrapper">
+            <button class="cart-item__counter__button">-</button>
+            <span class="cart-item__counter__quantity">{{quantity}} </span>
+            <button class="cart-item__counter__button">+</button>
+          </div>
+        </div>
+      </div>
+      <div class="cart-item__content__elements">
+        <div class="cart-item__close-button__wrapper">
+          <button class="cart-item__close-button">
+            &#10006;
+          </button>
+        </div>
+        <div class="cart-item__total-price">total price</div>
+      </div>
     </div>
   </div>
 </template>
@@ -20,6 +44,16 @@ export default {
       },
     },
   },
+  methods: {
+    showQuantity() {
+      console.log(this.cartItemData.quantity);
+    },
+  },
+  // computed: {
+  //   quantity() {
+  //     return this.$store.getters.PRODUCT_QUANTITY(this.product);
+  //   },
+  // },
 };
 </script>
 
--- src/store/test.js
+++ src/store/test.js
@@ -3,16 +3,34 @@ import axios from 'axios';
 export default {
   state: {
     products: [],
-    cart: [],
+    cart: [
+      {
+        image: 'w1.webp',
+        title: 'Твидовый кардиган из хлопка',
+        price: '7490',
+        sale: '',
+        sale_oldPrice: '',
+        category: 'Женщинам',
+        id: 1,
+      },
+    ],
   },
 
   getters: {
+    // PRODUCT_QUANTITY: (state, product) => {
+    //   const item = state.cart.find(i, i.id === product.id);
+    //   if (item) {
+    //     return item.quantity;
+    //   } else {
+    //     return null;
+    //   }
+    // },
     PRODUCTS(state) {
       return state.products;
     },
     CART(state) {
       return state.cart;
-    }
+    },
   },
 
   actions: {
@@ -24,18 +42,25 @@ export default {
         return products;
       });
     },
-    ADD_TO_CART({commit}, product) {
-      commit('SET_CART', product);
-    }
+    ADD_TO_CART({ commit }, product) {
+      commit('SET_TO_CART', product);
+    },
   },
 
   mutations: {
     SET_PRODUCTS_TO_STATE: (state, products) => {
       state.products = products;
     },
-    SET_CART: (state, product) => {
+    SET_TO_CART: (state, product) => {
       state.cart.push(product);
+
+    //   let item = state.cart.find(i => i.id === product.id)
+    //   if(item){
+    //     item.quantity++
+    //   } else {
+    //     state.cart.push({...product, quantity: 1})
+    //   }
+    // },
     }
-   
   },
 };
